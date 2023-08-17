b45466afe1b57b1c36ce1d0be7ce461375f5c3ca
sifuna2002 <sifunadone4704@gmail.com>
2023-08-17 10:29:44 +0300

    cofee

--- components/Home/SomethingIveBuilt/SomethingIveBuilt.tsx
+++ components/Home/SomethingIveBuilt/SomethingIveBuilt.tsx
@@ -453,6 +453,70 @@ export default function SomethingIveBuilt() {
             </div>
           </div>
         </div>
+                {/* // ?  Project 7 */}
+        <div data-aos="fade-up" className="relative md:grid md:grid-cols-12 w-full md:h-96  ">
+          {/* Left image */}
+          <div
+            className="hidden bg-AAprimary z-10  py-4 
+          absolute md:grid grid-cols-12 w-full h-full  content-center "
+          >
+            <div className="relative rounded w-full h-full col-start-6 col-span-7 ">
+              <a href="https://github.com/sifuna2002/cofee-app-ui" target={"_blank"} rel="noreferrer">
+                <div
+                  className="absolute w-full h-full rounded bg-AAprimary 
+           transition-opacity opacity-60 hover:opacity-0 hover:cursor-pointer duration-300"
+                ></div>
+              </a>
+
+              <Img src={"/cofee.jpg"} alt={"Project Screen shot"} className={`w-full rounded h-full `} />
+            </div>
+          </div>
+
+          {/* right Content */}
+          <div className=" md:absolute py-4  md:grid md:grid-cols-12 w-full h-full  content-center ">
+            {/* background for text in mobile responsive */}
+            <div className="absolute w-full h-full bg-opacity-70 z-0 md:order-2">
+              <div className="relative w-full h-full">
+                <div className="absolute w-full h-full bg-AAprimary opacity-50 z-10"></div>
+                <div className="absolute w-full h-full bg-AAprimary opacity-80 z-10"></div>
+                <Img src={"/cofee.jpg"} alt={"Project Screen shot"} className={`w-full h-full`} />
+              </div>
+            </div>
+
+            <div
+              className="px-8 pt-8 sm:pt-12 md:py-0 xl:col-span-6   
+            col-span-8 flex flex-col items-start  sm:space-y-3 md:order-1"
+            >
+              <div className="flex flex-col space-y-1  z-10">
+                <span className="text-AAsecondary text-base">Recent Project</span>
+                <a href="https://github.com/sifuna2002/cofee-app-ui" target={"_blank"} rel="noreferrer">
+                  <span className=" md:text-gray-200 text-AAsecondary font-bold text-xl hover:cursor-pointer">
+                    Cofee app UI
+                  </span>
+                </a>
+              </div>
+              <div className="w-full md:bg-AAtertiary rounded-md py-6 md:p-6  z-10">
+                <p className="text-gray-300 md:text-gray-400 text-left ">
+                  A <span className="text-AAsecondary">Cofee app UI</span> project that is both for
+                  <span className="text-AAsecondary"> Android </span>and <span className="text-AAsecondary">IOS</span> . Designed by <span className="text-AAsecondary">React Native</span> and 
+                  <span className="text-AAsecondary">Tailwind</span>. The project can be develop further to suit a coffee shop needs of selling coffee
+                </p>
+              </div>
+              <ul
+                className="flex flex-wrap w-full text-gray-300 md:text-gray-400
+               text-sm font-Text2 md:justify-start"
+              >
+                <span className="pr-4 z-10">React Native</span>
+                <span className="pr-4 z-10">Tailwind</span>
+                <span className="pr-4 z-10">Cofee</span>
+                <span className="pr-4 z-10">Shop</span>
+              </ul>
+              <div className="z-10 flex fle-row space-x-5 sm:pt-0 pt-2">
+                <GithubIcon link="https://github.com/sifuna2002/cofee-app-ui" />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
