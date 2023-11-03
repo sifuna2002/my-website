50553db85d4382ab9f1259990bc2d0a83bbd2f8b
sifuna2002 <sifunadone4704@gmail.com>
2023-11-03 07:16:57 +0300

    login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,104 +1,35 @@
-import Header from "../components/Header/Header";
-import Startup from "../components/Header/StartupLogo/Startup";
-import MyName from "../components/Home/MyName/MyName";
-import { useContext, useEffect, useState, useRef } from "react";
-import SocialMediaArround from "../components/Home/SocialMediaArround/SocialMediaArround";
-import AboutMe from "../components/Home/AboutMe/AboutMe";
-import ThisCantBeReached from "../components/Home/ThisSiteCantBeReached/ThisCantBeReached";
-import WhereIHaveWorked from "../components/Home/WhereIHaveWorked/WhereIHaveWorked";
-import SomethingIveBuilt from "../components/Home/SomethingIveBuilt/SomethingIveBuilt";
-import GetInTouch from "../components/Home/GetInTouch/GetInTouch";
-import Footer from "../components/Footer/Footer";
-import AppContext from "../components/AppContextFolder/AppContext";
-import Aos from "aos";
-import "aos/dist/aos.css";
-import Head from "next/head";
-import ScreenSizeDetector from "../components/CustomComponents/ScreenSizeDetector";
-export default function Home() {
-  const [ShowElement, setShowElement] = useState(false);
-  const [ShowThisCantBeReached, setShowThisCantBeReached] = useState(true);
-  const [ShowMe, setShowMe] = useState(false);
-  // context Variable to clearInterval
-  const context = useContext(AppContext);
-  const aboutRef = useRef<HTMLDivElement>(null);
-  const homeRef = useRef<HTMLDivElement>(null);
+import React, {useState, useEffect} from "react";
+import db from "../firebase";
+import {collection,doc, deleteDoc, getDocs, updateDoc, setDoc} from "firebase/firestore";
+import OtpInput from 'react-otp-input';
 
-  useEffect(() => {
-    // remove the interval Cookie timer setter when
-    clearInterval(context.sharedState.userdata.timerCookieRef.current);
-    if (typeof window !== "undefined") {
-      // remove UserDataPuller project EventListeners
-      window.removeEventListener("resize", context.sharedState.userdata.windowSizeTracker.current);
-      window.removeEventListener("mousemove", context.sharedState.userdata.mousePositionTracker.current, false);
-      // remove Typing project EventListeners
-      window.removeEventListener("resize", context.sharedState.typing.eventInputLostFocus);
-      document.removeEventListener("keydown", context.sharedState.typing.keyboardEvent);
-    }
-    setTimeout(() => {
-      setShowElement(true);
-    }, 4500);
-
-    setTimeout(() => {
-      setShowThisCantBeReached(false);
-    }, 5400);
-    // ? INFORMATIONAL next function will show the component after changing the state of ShowMe
-    setTimeout(() => {
-      setShowElement(false);
-      setShowMe(true);
-      context.sharedState.finishedLoading = true;
-      context.setSharedState(context.sharedState);
-    }, 10400);
-  }, [context, context.sharedState]);
-
-  useEffect(() => {
-    Aos.init({ duration: 2000, once: true });
-  }, []);
-
-  console.log("Portfolio Rendered...");
-  const meta = {
-    title: "Sifuna Donnex - Software Engineer",
-    description: `I'm a Software engineer and Graphic DEsigner. Get in touch with me to know more.`,
-    image: "/titofCercle.png",
-    type: "website",
-  };
-  const isProd = process.env.NODE_ENV === "production";
-
+const Home = () => {
   return (
     <>
-      <Head>
-        <title>{meta.title}</title>
-        <meta name="robots" content="follow, index" />
-        <meta content={meta.description} name="description" />
-        <meta property="og:url" content={`https://sifunadonnex.vercel.app`} />
-        <link rel="canonical" href={`https://sifunadonnex.vercel.app`} />
-        <meta property="og:type" content={meta.type} />
-        <meta property="og:site_name" content="Sifuna Portfolio" />
-        <meta property="og:description" content={meta.description} />
-        <meta property="og:title" content={meta.title} />
-        <meta property="og:image" content={meta.image} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@sifunado" />
-        <meta name="twitter:title" content={meta.title} />
-        <meta name="twitter:description" content={meta.description} />
-        <meta name="twitter:image" content={meta.image} />
-      </Head>
-      <div className="relative snap-mandatory min-h-screen bg-AAprimary w-full ">
-        {context.sharedState.finishedLoading ? <></> : ShowThisCantBeReached ? <ThisCantBeReached /> : <></>}
-        {context.sharedState.finishedLoading ? <></> : ShowElement ? <Startup /> : <></>}
-        <Header finishedLoading={context.sharedState.finishedLoading} sectionsRef={homeRef} />
-        <MyName finishedLoading={context.sharedState.finishedLoading} />
-        <SocialMediaArround finishedLoading={context.sharedState.finishedLoading} />
-        {context.sharedState.finishedLoading ? <AboutMe ref={aboutRef} /> : <></>}
-        {context.sharedState.finishedLoading ? <WhereIHaveWorked /> : <></>}
-        {context.sharedState.finishedLoading ? <SomethingIveBuilt /> : <></>}
-        {context.sharedState.finishedLoading ? <GetInTouch /> : <></>}
-        {context.sharedState.finishedLoading ? (
-          <Footer githubUrl={"https://github.com/sifuna2002/my-website"} hideSocialsInDesktop={true} />
-        ) : (
-          <></>
-        )}
-        {!isProd && <ScreenSizeDetector />}
+    {/* getting started with wesmopay tailwind css*/}
+    <div className="h-screen flex p-2 justify-center items-center bg-[#ffffff]"
+     style={{
+      backgroundImage: "url('/shape.svg')",
+      backgroundRepeat: "no-repeat",
+      backgroundPosition: "center", 
+      backgroundSize: "cover"
+      
+    }}
+    >
+      <div className="bg-transparent p-4 flex flex-col justify-between gap-y-44 rounded md:w-1/2 w-full">
+        <div className="flex flex-col gap-y-4">
+          <h1 className="text-4xl text-[#3D404F] text-center font-bold mb-10">The most trusted platform for online payments</h1>
+          <h2 className="text-[#3D404F] text-2xl text-center uppercase">160 countries. 100 currencies. one account</h2>
+        </div>
+        <div>
+            <button className="w-full py-3 bg-[#3D404F] rounded text-white hover:bg-[#35394d]"
+            onClick={() => window.location.href="/account"}
+            >Get Started</button>
+        </div>
       </div>
+    </div>
     </>
-  );
+  )
 }
+
+export default Home
